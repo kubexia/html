32483e1801f40fcb3386deb6cec76e2377458a0f
Gabriel Puscuta <gabi@kubexia.com>
2016-03-21 17:21:51 +0200

    first commit

---
+++ ServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+namespace Kubexia\HTML;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    
+    protected $packageName = 'kubexiahtml';
+
+
+    public function boot() {
+        $this->handleConfigs();
+        // $this->handleMigrations();
+        // $this->handleViews();
+        // $this->handleTranslations();
+        // $this->handleRoutes();
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        // Bind any implementations.
+    }
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [];
+    }
+    private function handleConfigs() {
+        $configPath = __DIR__ . '/config/'.$this->packageName.'.php';
+        
+        $this->publishes([$configPath => config_path($this->packageName.'.php')]);
+        
+        $this->mergeConfigFrom($configPath, $this->packageName);
+    }
+    
+    private function handleTranslations() {
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'packagename');
+    }
+    
+    private function handleViews() {
+        $this->loadViewsFrom(__DIR__.'/../views', 'packagename');
+        
+        $this->publishes([__DIR__.'/../views' => base_path('resources/views/vendor/packagename')]);
+        
+    }
+    private function handleMigrations() {
+        $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
+    }
+    private function handleRoutes() {
+        include __DIR__.'/../routes.php';
+    }
+}
