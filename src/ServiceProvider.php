4f0fb2bd649b7a9936f9241c006bcaf4943b1737
Gabriel Puscuta <gabi@gabi-MS-1738>
2016-03-23 11:43:16 +0200

    assets handler

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -20,11 +20,21 @@ class ServiceProvider extends LaravelServiceProvider {
 
 
     public function boot() {
+        new \Twig_SimpleFunction('theme', function($string='',$parameters = [], $secure = null){
+            return url('themes/'.config('section').'/'.config('theme').'/'.$string,$parameters, $secure);
+        });
+        
         $this->handleConfigs();
+        
         // $this->handleMigrations();
+        
         $this->handleViews();
+        
         $this->handleTranslations();
+        
         // $this->handleRoutes();
+        
+        $this->handleAssets();
     }
     /**
      * Register the service provider.
@@ -42,6 +52,7 @@ class ServiceProvider extends LaravelServiceProvider {
     public function provides() {
         return [];
     }
+    
     private function handleConfigs() {
         $configPath = __DIR__ . '/../config/'.$this->packageName.'.php';
         
@@ -52,6 +63,8 @@ class ServiceProvider extends LaravelServiceProvider {
     
     private function handleTranslations() {
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', $this->packageName);
+        
+        $this->publishes([__DIR__.'/../resources/lang' => base_path('resources/lang/vendor/'.$this->packageName)]);
     }
     
     private function handleViews() {
@@ -60,10 +73,18 @@ class ServiceProvider extends LaravelServiceProvider {
         $this->publishes([__DIR__.'/../resources/views' => base_path('resources/views/vendor/'.$this->packageName)]);
         
     }
+    
     private function handleMigrations() {
         $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
     }
+    
     private function handleRoutes() {
         include __DIR__.'/../src/Http/routes.php';
+    }
+    
+    private function handleAssets(){
+        $this->publishes([
+            __DIR__.'/../public' => public_path(),
+        ], 'kubexia');
     }
 }
